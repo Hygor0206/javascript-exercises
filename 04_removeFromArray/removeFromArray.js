dc449b0f20d028fe01d97dc913ee1ce3e66a0434
Albert <jxjfifa@gmail.com>
2023-05-24 23:54:43 -0300

    Exercicios 1,2,3 concluidos

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,10 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(numbers, param1, param2) {
+    for(let i=0;i<numbers.length;i++) {
+        if(param2===numbers[i]) {
+            numbers.splice(i, 1);
+        }
+    }
+    return numbers;
 };
 
 // Do not edit below this line
