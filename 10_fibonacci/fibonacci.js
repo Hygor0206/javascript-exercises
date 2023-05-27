f708d3127b141830aaa89cc97bf0ba8b6936c19b
Albert <jxjfifa@gmail.com>
2023-05-27 14:48:06 -0300

    Finished the solution to exercise 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,14 @@
-const fibonacci = function() {
+const fibonacci = function(position) {
+    if((position<0)) return "OOPS";
+    if (position === 0) return 0;
 
+    let fibN=1, op=0;
+    for (let i=1; i < +position; i++) {
+        const lastN = fibN;
+        fibN+=op;
+        op=lastN;
+    }
+    return fibN;
 };
 
 // Do not edit below this line
