1ba489b709399cca859eea179c82580cd5c9a016
Albert <jxjfifa@gmail.com>
2023-05-27 13:45:08 -0300

    Finished the solution to exercise 9

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,8 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let re=/[^A-Za-z0-9]/g; // Its the same as "let re=/[\W_]/g;"
+    let lowReStr = str.toLowerCase().replace(re, '');
+    let reverseStr = lowReStr.split('').reverse().join('');
+    return reverseStr===lowReStr;
 };
 
 // Do not edit below this line
