58b37f4c80970c1c72aa0ebb6d7ef4d1492a1210
Albert <jxjfifa@gmail.com>
2023-05-27 13:49:31 -0300

    Some comments at exercise 09 to optimize the code

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -5,5 +5,11 @@ const palindromes = function (str) {
     return reverseStr===lowReStr;
 };
 
+// Here's a more optimized version of the code below
+// const palindromes = function (str) {
+//     let processedString = str.toLowerCase().replace(/[\W_]/g, '');
+//     return processedString.split('').reverse().join('') === processedString;
+// };
+
 // Do not edit below this line
 module.exports = palindromes;
