9cd8ebaeba76a624ae196c7be94b7b665c69641d
Albert <jxjfifa@gmail.com>
2023-05-27 18:11:00 -0300

    Completed  the solution to exercise 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,8 +1,9 @@
-const findTheOldest = function(age) {
-    let findAge=age.map((people) => people.yearOfDeath-people.yearOfBirth);
-    let currentYear = new Date().getFullYear();
-    let maxAge=0;
-    let index;
+const findTheOldest = (age) => {
+    let findAge=age.map((people) => people.yearOfDeath-people.yearOfBirth),
+        currentYear = new Date().getFullYear(),
+        maxAge=0,
+        index;
+    
     for(let i=0;i<findAge.length;i++){
         if(isNaN(findAge[i])){
             findAge[i]=currentYear-age[i].yearOfBirth;
